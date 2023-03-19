44baf89297eaa34d2660cb47525001fea929c11a
KimJiHeon <000224kjh@gmail.com>
2023-03-19 17:37:29 +0900

    Study

---
+++ src/main/java/Ex/EX3_7.java
@@ -0,0 +1,14 @@
+package Ex;
+
+public class EX3_7 {
+    public static void main(String[] args) {
+        int a;
+        float b;
+
+        a = (int) 123.45f;
+        b = 200;
+
+        System.out.printf("a의 값 ==> %d \n", a);
+        System.out.printf("b의 값 ==> %f \n", b);
+    }
+}
---
+++ src/main/java/Ex/Q1.java
@@ -0,0 +1,23 @@
+package Ex;
+
+import java.util.Scanner;
+
+public class Q1 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        int a, b, c, d;
+        int result;
+
+        System.out.print("첫번째 계산할 값을 입력하세요 ==>");
+        a = s.nextInt();
+        System.out.print("두번째 계산할 값을 입력하세요 ==>");
+        b = s.nextInt();
+        System.out.print("세번째 계산할 값을 입력하세요 ==>");
+        c = s.nextInt();
+        System.out.print("네번째 계산할 값을 입력하세요 ==>");
+        d = s.nextInt();
+        result = a + b - c * d;
+        System.out.printf("%d + %d - %d * %d = %d \n", a, b, c, d, result);
+    }
+}
