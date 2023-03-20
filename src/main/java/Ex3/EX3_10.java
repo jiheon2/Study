cd937204b4f396c8eeee6f7fa83527341b665cc6
KimJiHeon <000224kjh@gmail.com>
2023-03-20 23:38:37 +0900

    Study

---
+++ src/main/java/Ex3/EX3_10.java
@@ -0,0 +1,11 @@
+package Ex3;
+
+public class EX3_10 {
+    public static void main(String[] args) {
+        int a=100, b=200;
+        float result;
+        result = a/ b;
+
+        System.out.printf ("%f \n", result);
+    }
+}
---
+++ src/main/java/Ex3/EX3_14.java
@@ -0,0 +1,13 @@
+package Ex3;
+
+public class EX3_14 {
+    public static void main(String[] args) {
+        boolean boo1, boo2;
+
+        boo1 = true;
+        System.out.printf("%s \n", boo1);
+
+        boo2 = (10 == 20);
+        System.out.printf("%s \n", boo2);
+    }
+}
