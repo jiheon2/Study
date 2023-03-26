bd5103bb5ec9812e98c882b47a36423929f7045a
KimJiHeon <000224kjh@gmail.com>
2023-03-26 16:34:57 +0900

    Study

---
+++ src/main/java/Ex4/EX4_6.java
@@ -0,0 +1,10 @@
+package Ex4;
+
+public class EX4_6 {
+    public static void main(String[] args) {
+        int a = 99;
+        System.out.printf(" AND연산 : %s \n", (a >= 100) && (a <= 200));
+        System.out.printf(" OR연산 : %s \n", (a >= 100) || (a <= 200));
+        System.out.printf(" NOT연산 : %s \n", !(a == 100));
+    }
+}
