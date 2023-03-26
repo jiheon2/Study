bf1e0626dc81569dcd5933144dcde2825df4ee54
KimJiHeon <000224kjh@gmail.com>
2023-03-26 16:21:24 +0900

    Study

---
+++ src/main/java/Ex4/EX4_3.java
@@ -0,0 +1,28 @@
+package Ex4;
+
+public class EX4_3 {
+    public static void main(String[] args) {
+        int a = 10;
+
+        a++;
+        System.out.printf("a++ ==> %d \n", a);
+
+        a--;
+        System.out.printf("a-- ==> %d \n", a);
+
+        a += 5;
+        System.out.printf("a += 5 ==> %d \n", a);
+
+        a -= 5;
+        System.out.printf("a -= 5 ==> %d \n", a);
+
+        a *= 5;
+        System.out.printf("a *= 5 ==> %d \n", a);
+
+        a /= 5;
+        System.out.printf("a /= 5 ==> %d \n", a);
+
+        a %= 5;
+        System.out.printf("a %%= 5 ==> %d \n", a);
+    }
+}
