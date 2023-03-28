ea0c59f79637539fecdff58dea95eca5bd5dba4d
KimJiHeon <000224kjh@gmail.com>
2023-03-28 20:23:11 +0900

    Study

---
+++ src/main/java/Ex4/EX4_11.java
@@ -0,0 +1,21 @@
+package Ex4;
+
+public class EX4_11 {
+    public static void main(String[] args)
+
+    {
+        byte a = 'A', b;
+        byte mask = 0x0F;
+
+        System.out.printf(" %X & %X = %X \n", a, mask, a & mask);
+        System.out.printf(" %X | %X = %X \n", a, mask, a | mask);
+
+        mask = 'a' - 'A';
+        b = (byte) (a ^ mask);
+        System.out.printf("%c ^ %d = %c \n", a, mask, b);
+        a = (byte) (b ^ mask);
+        System.out.printf("%c ^ %d = %c \n", a, mask, b);
+    }
+
+
+}
